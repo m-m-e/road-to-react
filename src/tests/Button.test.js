bd95c83a1bc00fbde8353db2f92bb7dece186695
Megan Edwards <megan.edwards@osoco.es>
2019-11-04 15:31:04 +0100

    Add tests for components

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+// import Enzyme, { shallow } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
 import Button from '../components/Button';
 
+// Enzyme.configure({ adapter: new Adapter() });
+
 describe('Button', () => {
+  const children = 'give me more';
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button>Give Me More</Button>, div);
@@ -16,4 +22,11 @@ describe('Button', () => {
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  // it('shows children as button message', () => {
+  //   const element = shallow(
+  //   <Button { ...children } />
+  //   );
+  //   expect(element.find('.table-row').length).toBe(2);
+  //   });
 });
